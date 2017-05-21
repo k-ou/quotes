14e09287b8fd374fdbbc62371e97e1592bebc9fe
Lowell Bander <lowellbander@gmail.com>
2017-05-21 15:09:27 -0700

    simplify

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
 jQuery(document).ready(function ($) {
-    //trigger the animation - open modal window
+    // open modal
     $('section').on('click', function () {
-        $(this).css('z-index', 100);
-        $(this).css('position', 'fixed');
-        $(this).css('width', '100vw');
-        $(this).css('height', '100vh');
-        $(this).css('top', 0);
-        $(this).css('left', 0);
-        $(this).find('.quote_container').css('height', '100vh');
+        $(this).addClass('open');
         $(this).find('.overlay').css('opacity', 1);
-        $(this).find('.overlay').css('position', 'fixed');
-        $(this).find('.overlay').css('z-index', '101');
+        $(this).find('.quote_container').css('height', '100vh');
     })
 });
