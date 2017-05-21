08280dc89175b88b30f31b5c9cd50e5ec31b2bdf
Lowell Bander <lowellbander@gmail.com>
2017-05-21 15:43:28 -0700

    close modal via esc

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-jQuery(document).ready(function ($) {
+$(document).ready(() =>{
     // open modal
-    $('section').on('click', function () {
-        $(this).addClass('open');
-        $(this).find('.overlay').css('opacity', 1);
-        $(this).find('.quote_container').css('height', '100vh');
-    })
+    $('section').on('click', openModal);
 });
+
+$(document).keyup(e => {
+    const ESC = 27;
+    if (e.keyCode === ESC) {
+        closeModal();
+    }
+});
+
+function openModal() {
+    $(this).addClass('open');
+    $(this).find('.overlay').css('opacity', 1);
+    $(this).find('.quote_container').css('height', '100vh');
+}
+
+function closeModal() {
+    $('.open').find('.overlay').css('opacity', 0);
+    $('.open').find('.quote_container').css('height', '400px');
+    $('.open').removeClass('open');
+}
