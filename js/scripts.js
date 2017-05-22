9a1c44e52486684856b9b6414c2a05fc6661e94f
k-ou <kellio94@ucla.edu>
2017-05-22 15:31:00 -0700

    Improved open modal styling

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,10 @@ $(document).keyup(e => {
 });
 
 function styleModal() {
-    //    $('.open').find('.container_effects3').css('border', '20px white solid');
-    //    $('.open').find('.container_effects3').css('background-color', 'rgba(229, 166, 0, 0.6)');
     $('.open').find('.container_effects3').addClass('modal_effect');
 }
 
 function unstyleModal() {
-//    $('.open').find('.container_effects3').css('border', '0px');
-//    $('.open').find('.container_effects3').css('background-color', 'rgba(229, 166, 0, 1.0)');
     $('.open').find('.container_effects3').removeClass('modal_effect');
 }
 
