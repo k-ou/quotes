66037a4c0d4fd3d4f9e69f5d14af82732238f235
k-ou <kellio94@ucla.edu>
2017-05-22 12:18:46 -0700

    Added hover effects to all existing profiles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-$(document).ready(() =>{
+$(document).ready(() => {
     // open modal
     $('section').on('click', openModal);
+    $('.close').on('click', closeModal);
 });
 
 $(document).keyup(e => {
@@ -10,14 +11,28 @@ $(document).keyup(e => {
     }
 });
 
+function styleModal() {
+    //    $('.open').find('.container_effects3').css('border', '20px white solid');
+    //    $('.open').find('.container_effects3').css('background-color', 'rgba(229, 166, 0, 0.6)');
+    $('.open').find('.container_effects3').addClass('modal_effect');
+}
+
+function unstyleModal() {
+//    $('.open').find('.container_effects3').css('border', '0px');
+//    $('.open').find('.container_effects3').css('background-color', 'rgba(229, 166, 0, 1.0)');
+    $('.open').find('.container_effects3').removeClass('modal_effect');
+}
+
 function openModal() {
     $(this).addClass('open');
     $(this).find('.overlay').css('opacity', 1);
-    $(this).find('.quote_container').css('height', '100vh');
+    $(this).find('.profile_background').addClass('quote_expand');
+    styleModal();
 }
 
 function closeModal() {
+    unstyleModal();
     $('.open').find('.overlay').css('opacity', 0);
-    $('.open').find('.quote_container').css('height', '400px');
+    $('.open').find('.profile_background').removeClass('quote_expand');
     $('.open').removeClass('open');
 }
