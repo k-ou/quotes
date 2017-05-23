1b618005efe2f8d96c7c259b0b1999981a7ac909
k-ou <kellio94@ucla.edu>
2017-05-22 23:24:20 -0700

    Tried to get transitions and close button working. Close button now working, but reopens modal?

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 $(document).ready(() => {
     // open modal
-    $('section').on('click', openModal);
+    var section = $('section');
+    var isOpen = section.hasClass('open');
+    if (!isOpen) {
+        section.on('click', openModal);
+        debugger;
+    }
     $('.close').on('click', closeModal);
 });
 
 $(document).keyup(e => {
     const ESC = 27;
+    const left = 37;
+    const right = 39;
     if (e.keyCode === ESC) {
+        closeModal();
+    } else if (e.keyCode === left) {
+        closeModal();
+    } else if (e.keyCode === right) {
         closeModal();
     }
 });
@@ -21,14 +32,17 @@ function unstyleModal() {
 
 function openModal() {
     $(this).addClass('open');
-    $(this).find('.overlay').css('opacity', 1);
+    $(this).find('.overlay').addClass('show_overlay');
     $(this).find('.profile_background').addClass('quote_expand');
+    $('body').css('overflow', 'hidden');
     styleModal();
 }
 
 function closeModal() {
+    debugger;
     unstyleModal();
-    $('.open').find('.overlay').css('opacity', 0);
+    $('.open').find('.overlay').removeClass('show_overlay');
     $('.open').find('.profile_background').removeClass('quote_expand');
     $('.open').removeClass('open');
+    $('body').css('overflow', 'visible');
 }
